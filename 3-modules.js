2452f98ab2bda122a64c0487823bf8f267914763
Diego Osorio <dieosorio@outlook.com>
2022-07-19 08:58:31 -0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+// Modules - Encapsulated Code (only share minimum)
+// CommonJS, every file is module (by default)
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+
+const { john, peter } = names;
+
+sayHi("susan");
+sayHi(john);
+sayHi(peter);
